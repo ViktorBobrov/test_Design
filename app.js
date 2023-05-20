e70e1050e2e724a111ba547cfde30264e2f4c1fb
ViktorBobrov <bobrov.voktor.93@gmail.com>
2023-05-20 19:19:19 +0700

    fix:adaptive

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ let swiper = new Swiper(".mySwiper", {
 	mousewheel: true,
 	keyboard: true,
  });
+
+ /*бургер меню*/
+ const burger = document.querySelector('.burger');
+ const nav = document.querySelector('.nav-links');
+ 
+ burger.addEventListener('click', () => {
+	nav.classList.toggle('nav-active');
+ 
+	burger.classList.toggle('toggle');
+ });
